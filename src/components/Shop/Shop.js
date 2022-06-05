3cabb7b5f80d4ce9940e786fa2066f003f198ee9
srrakib00 <srrakib00@gmail.com>
2022-06-05 22:08:04 +0600

    Product css and js update

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
@@ -13,7 +14,12 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <h2>Porducts:{products.length}</h2>
+             {
+                 products.map(product=><Product
+                 key={product.id}
+                 product={product}
+                 ></Product>)
+             }
 
             </div>
             <div className="cart-container">
